86a619e8abe6b84baabe7ced94c32919530821bc
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2021-08-16 10:11:17 +0530

    changed position of whitenoise lines in settings.py

--- userproject/settings.py
+++ userproject/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic', 
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
     'crispy_forms',
 ]
 
